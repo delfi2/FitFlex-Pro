5b4f6940079d4ea63313f29f90b5077ad12385f1
delfiarrigo <darrigo26@ucema.edu.ar>
2024-06-23 18:21:48 -0300

    Insertar archivo

--- server_ejercicio_poo.py
+++ server_ejercicio_poo.py
@@ -1,67 +1,61 @@
-from fit_db import get_db
-from class_ejercicio import ejercicio
+from flask import Flask, jsonify, request
+import ejercicio_controller_poo
+from fit_db import create_tables
+
+app = Flask(__name__)
 
 
-def insert_ejercicio(ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad):
-    db = get_db()
-    cursor = db.cursor()
-    statement = "INSERT INTO fit (ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad)     VALUES ( ?, ?, ?, ? ,?, ?, ?, ?)"
-    cursor.execute(statement, [ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad])
-    db.commit()
-    return True
-
-def update_ejercicio(ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad):
-    db = get_db()
-    cursor = db.cursor()
-    statement = "UPDATE fit SET ejercicio = ?, repeticiones = ?, tiempo= ?, peso= ?, fortalece= ?, number_pages= ?, press= ? WHERE ID = ?"
-    cursor.execute(statement, [ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad, ID])
-    db.commit()
-    return True
-
-
-def delete_ejercicio(ID):
-    db = get_db()
-    cursor = db.cursor()
-    statement = "DELETE FROM fit WHERE ID = ?"
-    cursor.execute(statement, [ID])
-    db.commit()
-    return True
-
-
-def get_by_id(ID):
-    db = get_db()
-    cursor = db.cursor()
-    statement = "SELECT ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad FROM fit WHERE ID = ?"
-    cursor.execute(statement, [ID])
-    single_ejercicio = cursor.fetchone()
-    ID = single_ejercicio[0]
-    ejercicio = single_ejercicio[1]
-    repeticiones = single_ejercicio[2]
-    tiempo = single_ejercicio[3]
-    peso = single_ejercicio[4]
-    fortalece = single_ejercicio[5]
-    serie = single_ejercicio[6]
-    dificultad = single_ejercicio[7]
-    ejercicio = ejercicio(ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad)
-    return ejercicio.serialize_details()
-
-
+@app.route('/ejercicio', methods=["GET"])
 def get_ejercicio():
-    db = get_db()
-    cursor = db.cursor()
-    query = "SELECT ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad FROM fit"
-    cursor.execute(query)
-    ejercicio_list = cursor.fetchall()
-    list_of_ejercicios=[]
-    for ejercicio in list_of_ejercicios:
-        ID = ejercicio[0]
-        ejercicio = ejercicio[1]
-        repeticiones = ejercicio[2]
-        tiempo = ejercicio[3]
-        peso = ejercicio[4]
-        fortalece = ejercicio[5]
-        serie = ejercicio[6]
-        dificultad = ejercicio[7]
-        ejercicio_to_add = ejercicio(ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad)
-        list_of_ejercicios.append(ejercicio_to_add)
-    return list_of_ejercicios
+    ejercicios = server_ejercicio_poo.get_ejercicio()
+    ejercicios_list = []
+    for ejercicio in ejercicios:
+        elem = ejercicios.serialize()
+        ejercicios_list.append(elem)
+    return jsonify(ejercicios_list)
+
+@app.route("/ejercicio/create", methods=["POST"])
+def insert_ejercicio():
+    ejercicio_details = request.get_json()
+    ID = ejercicio_details["ID"]
+    ejercicio = ejercicio_details["ejercicio"]
+    repeticiones =ejercicio_details["repeticiones"]
+    tiempo = ejercicio_details["tiempo"]
+    peso = ejercicio_details["peso"]
+    fortalece = ejercicio_details["fortalece"]
+    serie = ejercicio_details["serie"]
+    dificultad = ejercicio_details ["dificultad"]
+    result = server_ejercicio_poo.insert_ejercicio(ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad)
+    return jsonify(result)
+
+
+@app.route("/ejercicio/modify", methods=["PUT"])
+def update_ejercicio():
+    ejercicio_details = request.get_json()
+    ID = ejercicio_details["ID"]
+    ejercicio = ejercicio_details["ejercicio"]
+    repeticiones =ejercicio_details["repeticiones"]
+    tiempo = ejercicio_details["tiempo"]
+    peso = ejercicio_details["peso"]
+    fortalece = ejercicio_details["fortalece"]
+    serie = ejercicio_details["serie"]
+    dificultad = ejercicio_details ["dificultad"]
+    result = server_ejercicio_poo.update_ejercicio(ID, ejercicio, repeticiones, tiempo, peso, fortalece, serie, dificultad)
+    return jsonify(result)
+
+
+@app.route("/ejercicio/eliminate/<ID>", methods=["DELETE"])
+def delete_ejercicio(ID):
+    result = server_ejercicio_poo.delete_ejercicio(ID)
+    return jsonify(result)
+
+
+@app.route("/ejercicio/<ID>", methods=["GET"])
+def get_ejercicio_by_id(ID):
+    ejercicio = server_ejercicio_poo.get_by_id(ID)
+    return jsonify(ejercicio)
+
+create_tables()
+
+if __name__ == '__main__':
+    app.run()
