5dd844d86ddfdf99fc89586ef8dfa4d0874778c0
Delfina Arrigo <164285915+delfi2@users.noreply.github.com>
2024-06-25 17:02:03 -0300

    Update class_ejercicio.py

--- class_ejercicio.py
+++ class_ejercicio.py
@@ -5,31 +5,14 @@ class ejercicio:
         self.ejercicio = ejercicio
         self.dificultad = dificultad
 
-
-    def serialize(self):
-        return {
-            'ID': self.ID,
-            'ejercicio': self.ejercicio,
-            'repeticiones': self.repeticiones,
-            'serie': self.serie
-        }
-
-    def serialize_details(self):
-        return {
-            'ID': self.ID,
-            'ejercicio': self.ejercicio,
-            'repeticiones': self.repeticiones,
-            'tiempo': self.tiempo,
-            'peso': self.peso,
-            'fortalece': self.fortalece,
-            'serie': self.serie,
-            'dificultad': self.dificultad
-        }
 class ejercicio_gym:
      def __init__(self, ID, ejercicio, dificultad, repeticiones, series, peso) -> None:
          super().__init__(repeticiones, series, peso)
          self.repeticiones = repeticiones
          self.series = series
          self.peso = peso
-    
-
+class ejercicio_run:
+     def __init__(self, ID, ejercicio, dificultad, distancia, tiempo) -> None:
+         super().__init__(repeticiones, series, peso)
+         self.distancia = distancia
+         self.tiempo = series
