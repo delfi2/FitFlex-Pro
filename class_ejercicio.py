2d6aa2ad806bc450cde64e2b99ad1b09214a80ca
delfiarrigo <darrigo26@ucema.edu.ar>
2024-06-26 17:56:55 -0300

    Insertar archivo

--- class_ejercicio.py
+++ class_ejercicio.py
@@ -1,30 +1,77 @@
-class ejercicios:
+class Ejercicios:
     def __init__(self, ID, ejercicio, dificultad) -> None:
         self.ID = ID
         self.ejercicio = ejercicio
         self.dificultad = dificultad
 
+    def serialize(self):
+        return {
+            'ID': self.ID,
+            'ejercicio': self.ejercicio,
+        }
+
+    def serialize_detail(self):
+        return {
+            'ID': self.ID,
+            'ejercicio': self.ejercicio,
+            'dificultad': self.dificultad
+        }
+
     def __describir(self):
         return f"Ejercicio: {self.ejercicio}, Dificultad: {self.dificultad}"
 
-class EjercicioGym(ejercicios):
+
+class EjercicioGym(Ejercicios):
     def __init__(self, ID, ejercicio, dificultad, repeticiones, serie, peso) -> None:
         super().__init__(ID, ejercicio, dificultad)
         self.repeticiones = repeticiones
         self.serie = serie
         self.peso = peso
 
+    def serialize(self):
+        return {
+            'ID': self.ID,
+            'ejercicio': self.ejercicio,
+            'repeticiones': self.repeticiones,
+        }
+
+    def serialize_detail(self):
+        return {
+            'ID': self.ID,
+            'ejercicio': self.ejercicio,
+            'dificultad': self.dificultad,
+            'repeticiones': self.repeticiones,
+            'serie': self.serie,
+            'peso': self.peso
+        }
+
     def __describir(self):
         descripcion_base = super().describir()
         return f"{descripcion_base}, Repeticiones: {self.repeticiones}, Serie: {self.serie}, Peso: {self.peso}kg"
 
-class EjercicioRun(ejercicios):
+
+class EjercicioRun(Ejercicios):
     def __init__(self, ID, ejercicio, dificultad, distancia, tiempo) -> None:
         super().__init__(ID, ejercicio, dificultad)
         self.distancia = distancia
         self.tiempo = tiempo
 
+    def serialize(self):
+        return {
+            'ID': self.ID,
+            'ejercicio': self.ejercicio,
+            'distancia': self.distancia
+        }
+
+    def serialize_detail(self):
+        return {
+            'ID': self.ID,
+            'ejercicio': self.ejercicio,
+            'dificultad': self.dificultad,
+            'distancia': self.distancia,
+            'tiempo': self.tiempo
+        }
+
     def __describir(self):
         descripcion_base = super().describir()
         return f"{descripcion_base}, Distancia: {self.distancia}km, Tiempo: {self.tiempo}min"
-
